fa639671b118fc4a918ba846217d5171ebc14d5e
ajrullllllll <daniajril757@gmail.com>
2026-05-14 20:15:04 +0700

    Update wallet-config.ts

--- app/wallet-config.ts
+++ app/wallet-config.ts
@@ -1,17 +1,1 @@
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
-import { mainnet, polygon } from 'wagmi/chains'
 
-const projectId = 'ISI_PROJECT_ID_KAMU'
-
-const metadata = {
-  name: 'Wynex App',
-  description: 'Web3 App',
-  url: 'http://localhost:3000',
-  icons: ['https://avatars.githubusercontent.com/u/37784886']
-}
-
-export const config = defaultWagmiConfig({
-  chains: [mainnet, polygon],
-  projectId,
-  metadata,
-})
