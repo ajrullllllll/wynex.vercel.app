ee97cb5e2ad67bd38e8d2c26eb5e65eea1569c93
ajrullllllll <daniajril757@gmail.com>
2026-05-14 20:15:50 +0700

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,17 +1,1 @@
-'use client'
 
-import { WagmiProvider } from 'wagmi'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { wagmiAdapter } from './wallet-config'
-
-const queryClient = new QueryClient()
-
-export default function Providers({ children }) {
-  return (
-    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
-}
